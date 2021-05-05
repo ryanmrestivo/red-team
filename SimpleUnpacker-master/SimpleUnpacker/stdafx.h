411869725c8553173d056c1e744861dd9e8333a1
ryanmrestivo <53523282+ryanmrestivo@users.noreply.github.com>
2021-05-05 07:57:47 -0400

    .

---
+++ SimpleUnpacker-master/SimpleUnpacker/stdafx.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+
+#include <string>
+#include <map>
+#include <list>
+
+#include <windows.h>
+#include <TlHelp32.h>
