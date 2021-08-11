3104c7c8ae8d78a633b73e45b5c29b8e20f92d44
ryanmrestivo <53523282+ryanmrestivo@users.noreply.github.com>
2021-08-11 15:23:56 -0400

    .

--- Powershell+cmd/Revoke-Obfuscation/Requirements/CommandLine/Convert-PowerShellCommandLine.ps1
+++
@@ -1,5 +0,0 @@
-param($OriginalInput)
-
-$startInfo = New-Object System.Diagnostics.ProcessStartInfo -Property @{ RedirectStandardOutput = $true; FileName = "$PSScriptRoot\ConvertToPowerShellExpression.exe"; Arguments = $OriginalInput; UseShellExecute = $false }
-$process = [System.Diagnostics.Process]::Start($startInfo)
-$process.StandardOutput.ReadToEnd().Trim()
--- Powershell+cmd/Revoke-Obfuscation/Whitelist/Scripts_To_Whitelist/68DE420A4E77FCC92F46920D3097E5277DDABFB88D9438D014AEEB62735BED79.ps1
+++
@@ -1,1 +0,0 @@
-{ $_.GetMethod("AnalyzeAst") }
