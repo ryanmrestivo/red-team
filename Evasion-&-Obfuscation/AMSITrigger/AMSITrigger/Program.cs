145fb2c9f4dd7e426f59ffe34654894fd8139f81
Ryan Restivo <53523282+ryanmrestivo@users.noreply.github.com>
2022-12-10 20:16:26 -0500

    asmi trigger

---
+++ Evasion-&-Obfuscation/AMSITrigger/AMSITrigger/Program.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.IO;
+using NDesk.Options;
+
+namespace AmsiTrigger
+{
+
+    using static Globals;
+
+    public static class Globals
+    {
+        public static int minSignatureLength = 6;       // Playing with these can result in quicker execution time and less AMSIScanBuffer calls. It can also reduce the accuracy of trigger identification.
+        public static int maxSignatureLength = 2048;    // Setting maxSignatureLength will ensure that signatures split over data chunks dont get missed as only the first (chunkSize - maxSignatureLength) will be reported as clean
+        public static int format = 1;
+        public static int chunkSize = 4096;
+        public static int max = 0;
+        public static Boolean help = false;
+        public static Boolean debug = false;
+        public static string inScript;
+        public static IntPtr amsiContext;
+        public static string inURL;
+        public static int lineNumber = 1;
+        public static int sampleIndex = 0; 
+        public static int amsiCalls = 0;
+        public static int chunksProcessed = 0;
+        public static int triggersFound = 0;
+    }
+
+
+
+
+    class Program
+    {
+                          
+        [DllImport("Amsi.dll", EntryPoint = "AmsiInitialize", CallingConvention = CallingConvention.StdCall)]
+        public static extern int AmsiInitialize([MarshalAs(UnmanagedType.LPWStr)]string appName, out IntPtr amsiContext);
+        [DllImport("Amsi.dll", EntryPoint = "AmsiUninitialize", CallingConvention = CallingConvention.StdCall)]
+        public static extern void AmsiUninitialize(IntPtr amsiContext);
+  
+                            
+
+
+        static void Main(string[] args)
+        {
+            
+            string infile = string.Empty;
+
+
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+
+            if (!validParameters(args))
+            {
+                return;
+            }
+
+            if (!AmsiInitialize())
+            {
+                return;
+            }
+
+            Triggers.FindTriggers();
+
+            AmsiUninitialize(amsiContext);
+
+            watch.Stop();
+
+            if (debug)
+            {
+                Console.ForegroundColor = System.ConsoleColor.Gray;
+                Console.WriteLine($"\n\r\n\rChunks Processed: {chunksProcessed}");
+                Console.WriteLine($"Triggers Found: {triggersFound}");
+                Console.WriteLine($"AmsiScanBuffer Calls: {amsiCalls}");
+                Console.WriteLine($"Total Execution Time: {watch.Elapsed.TotalSeconds} s");
+            }
+
+        }
+
+
+
+        public static Boolean validParameters(string[] args)
+        {
+
+
+            var options = new OptionSet(){
+                {"i|inputfile=", "Powershell filename or", o => inScript = o},
+                {"u|url=", "URL eg. https://10.1.1.1/Invoke-NinjaCopy.ps1", o => inURL = o},
+                {"f|format=", "Output Format:"+"\n1 - Only show Triggers\n2 - Show Triggers with line numbers\n3 - Show Triggers inline with code\n4 - Show AMSI calls (xmas tree mode)", (int o) => format = o},
+                {"d|debug","Show debug info", o => debug = true},
+                {"m|maxsiglength=","Maximum Signature Length to cater for, default=2048", (int o) => maxSignatureLength = o},
+                {"c|chunksize=","Chunk size to send to AMSIScanBuffer, default=4096", (int o) => chunkSize = o},
+                {"h|?|help","Show Help", o => help = true},
+            };
+
+            try
+            {
+                options.Parse(args);
+
+                if (help || args.Length == 0)
+                {
+                    showHelp(options);
+                    return false;
+                }
+
+                if (format < 1 || format > 4)
+                {
+                    showHelp(options);
+                    return false;
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine(e.Message);
+                showHelp(options);
+                return false;
+            }
+
+
+            if (inScript!=null && inURL!=null)
+            {
+                Console.WriteLine("[+] Supply either -i or -u, not both");
+                return false;
+            }
+
+            if (inURL!=null && inURL.ToLower().Substring(0,7)!="http://" && inURL.ToLower().Substring(0, 8) != "https://") 
+            {
+                Console.WriteLine("[+] Invalid URL - must begin with http:// or https://");
+                return false;
+            }
+
+            if (chunkSize < maxSignatureLength)
+            {
+                Console.WriteLine("[+] chunksize should always be > maxSignatureLength");
+                return false;
+            }
+
+
+            if (inScript!=null && !File.Exists(inScript))
+            {
+                Console.WriteLine("[+] File not found");
+                return false;
+            }
+            return true;
+        }
+
+
+        public static void showHelp(OptionSet p)
+        {
+
+            Console.WriteLine(@"     _    __  __ ____ ___ _____     _");
+            Console.WriteLine(@"    / \  |  \/  / ___|_ _|_   _| __(_) __ _  __ _  ___ _ __ ");
+            Console.WriteLine(@"   / _ \ | |\/| \___ \| |  | || '__| |/ _` |/ _` |/ _ \ '__|");
+            Console.WriteLine(@"  / ___ \| |  | |___) | |  | || |  | | (_| | (_| |  __/ |   ");
+            Console.WriteLine(@" /_/   \_\_|  |_|____/___| |_||_|  |_|\__, |\__, |\___|_|   ");
+            Console.WriteLine(@"                                      |___/ |___/         v3");
+            Console.WriteLine("@_RythmStick\n\n\n");
+
+           
+            Console.WriteLine("Show triggers in Powershell file or URL.\nUsage:");
+            p.WriteOptionDescriptions(Console.Out);
+        }
+
+
+        public static Boolean AmsiInitialize()
+        {
+            
+            int returnValue = AmsiInitialize(@"PowerShell_C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe_10.0.18362.1", out amsiContext);
+            if (returnValue==0)
+            {
+                return true;
+            }
+            return false;
+        }
+   
+    }
+}
+
+
---
+++ Evasion-&-Obfuscation/AMSITrigger/AMSITrigger/Triggers.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace AmsiTrigger
+{
+
+    using static Globals;
+    public class Triggers
+    {
+        [DllImport("Amsi.dll", EntryPoint = "AmsiScanBuffer", CallingConvention = CallingConvention.StdCall)]
+        private static extern int AmsiScanBuffer(IntPtr amsiContext, byte[] buffer, uint length, string contentName, IntPtr session, out AMSI_RESULT result);
+
+        private enum AMSI_RESULT
+        {
+            AMSI_RESULT_CLEAN = 0,
+            AMSI_RESULT_NOT_DETECTED = 1,
+            AMSI_RESULT_DETECTED = 32768
+        }
+
+        private static byte[] bigSample;
+        private static byte[] chunkSample;
+        private static int triggerStart = 0;
+        private static int triggerEnd;
+        private static int startIndex = 0;
+        public static void FindTriggers()
+        {
+            AMSI_RESULT result;
+            
+            if (!protectionEnabled(amsiContext))
+            {
+                return;
+            }
+
+
+            if (inScript != null)
+            {
+                bigSample = File.ReadAllBytes(inScript);
+            }
+            else
+            {
+                try
+                {
+                    WebClient client = new WebClient();
+                    client.Proxy = WebRequest.GetSystemWebProxy();
+                    client.Proxy.Credentials = CredentialCache.DefaultCredentials;
+                    ServicePointManager.SecurityProtocol = System.Net.SecurityProtocolType.Tls | (SecurityProtocolType)768 | (SecurityProtocolType)3072;
+                    bigSample = client.DownloadData(inURL);
+                }
+                catch (Exception e)
+                {
+                    Console.Error.WriteLine(e.Message);
+                    return;
+                }
+
+            }
+
+
+            result = scanBuffer(bigSample, amsiContext);
+            if (result != AMSI_RESULT.AMSI_RESULT_DETECTED)
+            {
+                Console.WriteLine(string.Format("[+] {0}", result));
+                return;
+            }
+
+
+
+
+            while (startIndex + AmsiTrigger.Globals.chunkSize < bigSample.Length)
+            {
+                chunkSample = new byte[AmsiTrigger.Globals.chunkSize];
+                Array.Copy(bigSample, startIndex, chunkSample, 0, AmsiTrigger.Globals.chunkSize);
+                processChunk(chunkSample);
+            }
+
+ 
+            while (startIndex < bigSample.Length)
+            {
+                chunkSample = new byte[bigSample.Length - startIndex];
+                Array.Copy(bigSample, startIndex, chunkSample, 0, chunkSample.Length);
+                processChunk(chunkSample);
+            }
+        }
+
+
+
+
+        private static void processChunk(byte[] chunkSample )
+        {
+            AMSI_RESULT result;
+
+            chunksProcessed++;
+            
+            result = scanBuffer(chunkSample, amsiContext);
+
+
+            if (result != AMSI_RESULT.AMSI_RESULT_DETECTED)  
+            {
+                if (chunkSample.Length > maxSignatureLength)
+                {
+                    showText(chunkSample, 0, AmsiTrigger.Globals.chunkSize - maxSignatureLength, false);
+                     startIndex += AmsiTrigger.Globals.chunkSize - maxSignatureLength;
+                } else
+                {
+                    showText(chunkSample, 0, chunkSample.Length, false); 
+                    startIndex+=chunkSample.Length;
+                }
+
+                return;
+            }
+            triggerEnd = findTriggerEnd() + 1;
+            triggerStart = findTriggerStart(triggerEnd);
+
+
+            triggersFound++;
+
+            showText(chunkSample, 0, triggerStart, false);
+            showText(chunkSample, triggerStart, triggerEnd-triggerStart, true);
+            startIndex += triggerEnd;
+            return;
+                       
+        }
+
+            private static int findTriggerEnd()
+        {
+
+            AMSI_RESULT result;
+            byte[] tmpSample;
+            int lastBytes;
+
+            for (int sampleIndex = 2; sampleIndex < chunkSample.Length + minSignatureLength; sampleIndex += minSignatureLength)
+            {
+                if (sampleIndex> chunkSample.Length) {
+                    sampleIndex = chunkSample.Length;
+                }
+                tmpSample = new byte[sampleIndex];
+                Array.Copy(chunkSample, 0, tmpSample, 0, sampleIndex);
+                result = scanBuffer(tmpSample, amsiContext);
+
+
+
+
+                if (result == AMSI_RESULT.AMSI_RESULT_DETECTED)
+                {
+
+                    for (lastBytes = 0; lastBytes < minSignatureLength; lastBytes++)
+                    {
+
+                        tmpSample = new byte[sampleIndex - lastBytes];
+                        Array.Copy(chunkSample, 0, tmpSample, 0, sampleIndex - lastBytes);
+                        result = scanBuffer(tmpSample, amsiContext);
+                        if (result != AMSI_RESULT.AMSI_RESULT_DETECTED)
+                        {
+                            return sampleIndex - lastBytes;
+                        }
+                    }
+                    return sampleIndex - lastBytes;
+                }
+            }
+            return 0;
+        }
+
+
+
+
+
+
+        private static int findTriggerStart(int triggerEnd)
+        {
+            AMSI_RESULT result;
+            byte[] tmpSample;
+
+            for (int sampleIndex = triggerEnd-1; sampleIndex > 0; sampleIndex--)
+            {
+
+                tmpSample = new byte[triggerEnd-sampleIndex];
+                Array.Copy(chunkSample, sampleIndex, tmpSample, 0, triggerEnd - sampleIndex);
+                string ssstring = Encoding.Default.GetString(tmpSample);
+                result = scanBuffer(tmpSample, amsiContext);
+
+                if (result == AMSI_RESULT.AMSI_RESULT_DETECTED)
+                {
+                    
+                    return sampleIndex;
+                }
+
+            }
+            
+            return 0;
+        }
+
+ 
+        private static void showText(byte[] output, int start, int length, Boolean highLight) 
+            {
+                
+                byte[] tmpSample = new byte[length];
+                Array.Copy(output, start, tmpSample, 0, length);
+
+            
+               switch (format)
+                    {
+
+
+                    case 1:
+                            if (highLight)
+                            {
+                                Console.ForegroundColor = System.ConsoleColor.Gray;
+                                Console.WriteLine("[+] \"" + Encoding.Default.GetString(tmpSample) + "\"");
+                    }
+                            break;
+                    case 2:
+                           if (highLight)
+                           {
+                                Console.ForegroundColor = System.ConsoleColor.Gray;
+                                Console.WriteLine("[" + lineNumber + "]\t\"" + Encoding.Default.GetString(tmpSample) + "\"");
+                           }
+                           break;
+
+                    case 3:
+                          if (highLight)
+                          { 
+                              Console.ForegroundColor = System.ConsoleColor.Red;
+                              Console.Write(Encoding.Default.GetString(tmpSample));
+                          }
+                          else
+                          {
+                              Console.ForegroundColor = System.ConsoleColor.Gray;
+                              Console.Write(Encoding.Default.GetString(tmpSample));
+                          }
+                          break;
+                    
+                    case 4:
+                        Console.ForegroundColor = System.ConsoleColor.Gray;
+                        Console.WriteLine(Encoding.Default.GetString(tmpSample));
+                        break;
+
+            }
+
+            if (format == 2) { lineNumber += returnsInSample(tmpSample, length);}
+
+        }
+
+
+
+        private static int returnsInSample(byte[] sample,int numBytes)
+        {
+            
+            return new Regex(@"\n").Matches(Encoding.Default.GetString(sample).Substring(0,numBytes)).Count;
+        }
+        private static AMSI_RESULT scanBuffer(byte[] sample, IntPtr amsiContext)
+        {
+            AMSI_RESULT result = 0;
+            int returnValue;
+            IntPtr session = IntPtr.Zero;
+
+            if (format==4)
+            {
+                showText(sample, 0, sample.Length, false);
+            }
+
+
+            returnValue = AmsiScanBuffer(amsiContext, sample, (uint)sample.Length, "Sample", IntPtr.Zero, out result);
+            amsiCalls++;
+            return result;
+        }
+
+        private static Boolean protectionEnabled(IntPtr amsiContext)
+        {
+
+            byte[] sample = Encoding.UTF8.GetBytes("AMSIScanBuffer");
+            AMSI_RESULT result = Triggers.scanBuffer(sample, amsiContext);
+
+            if (result == AMSI_RESULT.AMSI_RESULT_NOT_DETECTED)
+            {
+                Console.WriteLine("[+] Check Real Time protection is enabled");
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+
+        }
+
+    }
+}
+
